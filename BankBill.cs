6ceb118e137ece1639f6c17489604cae47621fa0
Serg <a_s_v86@mail.ru>
2022-01-04 19:56:06 +0300

    Initial

---
+++ BankBill.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBrains.OOP.Lesson2.Task1
+{
+    class BankBill
+    {
+        private long BillNumber;
+        private double Balance;
+        private static int LastBillNumber;
+        internal enum BillTypes{
+             CompanyBill,
+             PrivateBill
+        }
+        private BillTypes TheBillTypes;
+
+        internal long GetBillNumber()
+        {
+            return BillNumber;
+        }
+
+        internal double GetBalance()
+        {
+            return Balance;
+        }
+
+        internal BillTypes GetBillTypes()
+        {
+            return TheBillTypes;
+        }
+
+        internal long SetBillNumber()
+        {
+            return BillNumber = LastBillNumber+1;
+        }
+
+        internal double SetBalance(double value)
+        {
+            return Balance=value;
+        }
+
+        internal BillTypes SetBillTypes(BillTypes billTypes)
+        {
+            return TheBillTypes=BillTypes.CompanyBill;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBrains.OOP.Lesson2.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankBill bankBill = new BankBill();
+            Console.WriteLine(bankBill.SetBalance(1111));
+            Console.WriteLine(bankBill.SetBillNumber());
+            Console.WriteLine(bankBill.SetBillTypes(BankBill.BillTypes.CompanyBill));
+            Console.ReadKey();
+        }
+    }
+}
